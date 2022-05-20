6db5a9144d95b915b1aa0cf4b397cc28f577d214
Shouhardik Saha <60508616+shouhardik@users.noreply.github.com>
2022-05-20 23:59:10 +0530

    socket

---
+++ server.js
@@ -0,0 +1,37 @@
+const express=require('express')
+const app=express()
+const server=require('http').Server(app)
+const io=require('socket.io')(server)
+const {v4:uuidV4} = require('uuid')
+app.set('view engine','ejs')
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.redirect(`/${uuidV4()}`)
+    // console.log("hi")
+
+})
+
+app.get('/:room',(req,res)=>{
+    res.render('room',{roomId:req.params.room})
+   // console.log("ch")
+})
+
+io.on('connection',socket=>{
+    socket.on('join-room',(roomId,userId)=>{
+        
+        console.log(roomId,userId)
+        
+        socket.join(roomId)
+        
+       // io.to(roomId).emit('user-connected', userId)
+        socket.to(roomId).emit('user-connected', userId)
+
+        socket.on('disconnect',()=>{
+            socket.to(roomId).emit('user-disconnected',userId)
+        })
+        
+    })
+})
+
+server.listen(3000)
